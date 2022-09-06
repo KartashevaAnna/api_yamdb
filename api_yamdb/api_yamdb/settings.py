06b6e81a38dc9e73f5a7cb2f139b4c881d723ee8
anna kartasheva <annakartashevamail@gmail.com>
2022-09-06 11:36:59 +0300

    added apps users, reviews

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,7 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api'
+    'api',
+    'users',
+    'reviews',
 ]
 
 MIDDLEWARE = [
