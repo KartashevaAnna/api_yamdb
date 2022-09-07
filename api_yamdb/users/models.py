7f5e9c4dbbd670eebb3bcdfbe83322423294823d
anna kartasheva <annakartashevamail@gmail.com>
2022-09-07 17:46:17 +0300

    changed user model as per Alex

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,5 +1,18 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.tokens import default_token_generator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+USER = 'user'
+ADMIN = 'admin'
+MODERATOR = 'moderator'
+
+ROLE_CHOICES = [
+    (USER, USER),
+    (ADMIN, ADMIN),
+    (MODERATOR, MODERATOR),
+]
 
 
 class User(AbstractUser):
@@ -7,7 +20,45 @@ class User(AbstractUser):
         'Biography',
         blank=True,
     )
-    role = models.TextField(
-        'Role',
-        default='user',
+    role = models.CharField(
+        'role',
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default=USER,
+        blank=True
     )
+    confirmation_code = models.CharField(
+        'confirmation_code',
+        max_length=255,
+        null=True,
+        blank=False,
+        default='XXXX'
+    )
+    @property
+    def is_user(self):
+        return self.role == USER
+
+    @property
+    def is_admin(self):
+        return self.role == ADMIN
+
+    @property
+    def is_moderator(self):
+        return self.role == MODERATOR
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
+
+@receiver(post_save, sender=User)
+def post_save(sender, instance, created, **kwargs):
+    if created:
+        confirmation_code = default_token_generator.make_token(
+            instance
+        )
+        instance.confirmation_code = confirmation_code
+        instance.save()
