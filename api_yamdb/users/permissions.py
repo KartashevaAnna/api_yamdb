559c7e69aa3280233f3e31ba4a61a90f81136975
anna kartasheva <annakartashevamail@gmail.com>
2022-09-09 20:09:58 +0300

    14 tests passed

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -1,20 +1,5 @@
 from rest_framework import permissions
 
-#
-#
-# class AuthorOrReadOnly(permissions.BasePermission):
-#     def has_permission(self, request, view):
-#         return (
-#             request.method in permissions.SAFE_METHODS
-#             or request.user.is_authenticated
-#         )
-#
-# def has_object_permission(self, request, view, obj):
-#     return (
-#         request.method in permissions.SAFE_METHODS
-#         or obj.author == request.user
-#     )
-
 
 class AnonymousReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
@@ -44,15 +29,12 @@ class IsAdminUserOrReadOnly(permissions.BasePermission):
             or request.user.role == permissions.IsAdminUser
         )
 
-# class NotAuthorOrReadOnly(permissions.BasePermission):
-#     def has_permission(self, request, view):
-#         return (
-#             request.method in permissions.SAFE_METHODS
-#             or request.user.is_authenticated
-#         )
-#
-#     def has_object_permission(self, request, view, obj):
-#         return (
-#             request.method in permissions.SAFE_METHODS
-#             or obj.author != request.user
-#         )
+class IsMyselfOrAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user == self.user or request.user.role == permissions.IsAdminUser
+        )
